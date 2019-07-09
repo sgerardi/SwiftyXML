01e8b8b1a2cd80e00a464ec0a397fb1f17a3ad71
Sam Gerardi <sgerardi@seamgen.com>
2019-07-09 10:48:36 -0700

    Add targets.

--- Package.swift
+++ Package.swift
@@ -7,5 +7,12 @@ let package = Package(
         .library(
             name: "SwiftyXML",
             targets: ["SwiftyXML"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target defines a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SwiftyXML",
+            dependencies: [])
     ]
 )
